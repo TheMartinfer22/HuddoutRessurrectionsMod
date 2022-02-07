a06243303a0533f8bcfd0c51f14c3538f39d087e
Martinfer22 <martinsocial1706@gmail.com>
2022-02-07 20:19:08 -0300

    core: removed technology chest (!op items generation)

--- src/main/java/dev/nanosync/huddoutressurrections/manager/ModItems.java
+++ src/main/java/dev/nanosync/huddoutressurrections/manager/ModItems.java
@@ -3,14 +3,11 @@ package dev.nanosync.huddoutressurrections.manager;
 import dev.nanosync.huddoutressurrections.Main;
 import dev.nanosync.huddoutressurrections.item.chest.EternityChestItem;
 import dev.nanosync.huddoutressurrections.item.chest.MinersChestItem;
-import dev.nanosync.huddoutressurrections.item.chest.TechnologyChestItem;
 import dev.nanosync.huddoutressurrections.item.key.EternityKeyItem;
 import dev.nanosync.huddoutressurrections.item.key.MinersKeyItem;
-import dev.nanosync.huddoutressurrections.item.key.TechnologicalKeyItem;
 import dev.nanosync.huddoutressurrections.item.sword.HuddoutSwordItem;
 import dev.nanosync.huddoutressurrections.tab.ItensTab;
 import net.minecraft.item.*;
-import net.minecraftforge.common.ToolType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -22,11 +19,6 @@ public class ModItems {
     public static final RegistryObject<EternityKeyItem> ETERNITY_KEY = ITEMS.register("eternity_key", () ->
             new EternityKeyItem(new Item.Properties()
                     .rarity(Rarity.EPIC)
-                    .group(ItensTab.get)));
-
-    public static final RegistryObject<TechnologicalKeyItem> TECHNOLOGICAL_KEY = ITEMS.register("technology_key", () ->
-            new TechnologicalKeyItem(new Item.Properties()
-                    .rarity(Rarity.RARE)
                     .group(ItensTab.get)));
 
     public static final RegistryObject<MinersKeyItem> MINERS_KEY = ITEMS.register("miners_key", () ->
@@ -41,11 +33,6 @@ public class ModItems {
                     .maxStackSize(1)
                     .group(ItensTab.get)));
 
-    public static final RegistryObject<TechnologyChestItem> TECHNOLOGY_CHEST = ITEMS.register("technology_chest", () ->
-            new TechnologyChestItem(new Item.Properties()
-                    .rarity(Rarity.RARE)
-                    .maxStackSize(1)
-                    .group(ItensTab.get)));
 
     public static final RegistryObject<MinersChestItem> MINERS_CHEST = ITEMS.register("miners_chest", () ->
             new MinersChestItem(new Item.Properties()
